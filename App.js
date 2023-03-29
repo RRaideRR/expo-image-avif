3a8c85f91776d89170d4218a948da7513dcfe398
philipp <philipp.reiter90@gmail.com>
2023-03-29 09:44:26 +0200

    added avif, installed expo-image and displayed the avif file

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Image } from 'expo-image';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Image
+        source={require('./assets/test.avif')}
+        style={{ height: 200, width: 100 }}
+      />
     </View>
   );
 }
